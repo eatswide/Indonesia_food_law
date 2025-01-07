713188b8f50b268bb30b8baeed8f08d25061e939
eatswide <seoyoung@snu.ac.kr>
2025-01-07 16:02:55 +0900

    remove unused files

--- set_openai_api_key.py
+++
@@ -1,5 +0,0 @@
-# set_openai_api_key.py
-import os
-
-def set_openai_api_key():
-    os.environ["OPENAI_API_KEY"] = "yoursk-proj-P2_P5ZXLxGfziYBkj8vbKms3eIg_3fdkTtW2Bn0ku9ZHuXw8fqJgaYR0rZt6hVrS-XaR1cg8ZBT3BlbkFJOS8o9M-MSlTZygEtz5juiSEgIphCY0iZq5r9YNJfa3qkZ3YiLvDKw09W4kXr5X4zqFE43y-PwA"
